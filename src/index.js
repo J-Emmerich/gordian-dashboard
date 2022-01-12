007d7ef9c779156237fee079ec65d9e5a60e08ec
Joao Emmerich <emmerichjoao@protonmail.com>
2022-01-12 05:13:42 +0100

    deleted unused files

--- src/index.js
+++ src/index.js
@@ -1,16 +1,12 @@
 import { StrictMode } from "react";
 import ReactDOM from "react-dom";
 import Login from "./Login";
-
-
 import "typeface-roboto";
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
   <StrictMode>
-   
       <Login />
-   
   </StrictMode>,
   rootElement
 );
